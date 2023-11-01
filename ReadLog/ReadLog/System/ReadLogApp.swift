a41360f73fa23aef5777155338b3487c7abe95bf
SH0123 <sanghyoboy@naver.com>
2023-11-01 10:22:21 +0900

    Add: 프로젝트 구조 설정
    목적에 따라 프로젝트 폴더를 나눴습니다

---
+++ ReadLog/ReadLog/System/ReadLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadLogApp.swift
+//  ReadLog
+//
+//  Created by sanghyo on 2023/11/01.
+//
+
+import SwiftUI
+
+@main
+struct ReadLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
