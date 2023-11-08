cf9033b8301fb0d1d6a05ca1c3ab65f6fec53345
SH0123 <sanghyoboy@naver.com>
2023-11-08 18:44:05 +0900

    Feat: add extension
    Color+Extension, TextStyleExtension, color asset, font

---
+++ ReadLog/ReadLog/Extension/TextStyle+Extension.swift
@@ -0,0 +1,117 @@
+//
+//  TextStyle+Extension.swift
+//  ReadLog
+//
+//  Created by sanghyo on 11/8/23.
+//
+
+import SwiftUI
+
+struct Display: ViewModifier {
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(color)
+            .font(.custom("omyu pretty", size: 27))
+    }
+}
+
+struct Title: ViewModifier {
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(color)
+            .font(.custom("omyu pretty", size: 22))
+    }
+}
+
+
+struct BodyDefault: ViewModifier {
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("omyu pretty", size: 20))
+            .foregroundStyle(color)
+            .lineSpacing(15)
+            .padding(.vertical, 7.5)
+    }
+}
+
+
+struct Body1: ViewModifier {
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(color)
+            .font(.custom("omyu pretty", size: 18))
+    }
+}
+
+
+struct Body2: ViewModifier {
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(color)
+            .font(.custom("omyu pretty", size: 18))
+    }
+}
+
+
+struct Body3: ViewModifier {
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(color)
+            .font(.custom("omyu pretty", size: 16))
+    }
+}
+
+
+struct Mini: ViewModifier {
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(color)
+            .font(.custom("omyu pretty", size: 14))
+    }
+}
+
+
+
+extension View {
+    func display(_ color: Color) -> some View {
+        self.modifier(Display(color: color))
+    }
+    
+    func title(_ color: Color) -> some View {
+        self.modifier(Title(color: color))
+    }
+    
+    func bodyDefault(_ color: Color) -> some View {
+        self.modifier(BodyDefault(color: color))
+    }
+    
+    func body1(_ color: Color) -> some View {
+        self.modifier(Body1(color: color))
+    }
+    
+    func body2(_ color: Color) -> some View {
+        self.modifier(Body2(color: color))
+    }
+    
+    func body3(_ color: Color) -> some View {
+        self.modifier(Body3(color: color))
+    }
+    
+    func mini(_ color: Color) -> some View {
+        self.modifier(Mini(color: color))
+    }
+}
